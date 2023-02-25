42258bc7b144a875a328ff37c3f9f3646a78ee17
blck-snwmn <44711725+blck-snwmn@users.noreply.github.com>
2023-02-25 23:20:56 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,13 +1,9 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
-  fill(0);
-  textSize(width / 60);
-  textAlign(CENTER);
-  text("Move your mouse around the screen", width / 2, height / 3);
+  background(220);
 }
 
 function draw() {
   fill(mouseX, mouseY, mouseX + mouseY, 20);
-  ellipse(mouseX, mouseY, abs(pmouseX - mouseX) * 2, abs(pmouseX - mouseX) * 2);
+  rect(mouseX, mouseY, abs(pmouseX - mouseX) * 2, abs(pmouseX - mouseX) * 2);
 }
